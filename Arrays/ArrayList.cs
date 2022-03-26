fd66ab51d73d9cee37c13ddfe46fed89b05097f7
bazhenov-2001 <bazhenov-2001@mail.ru>
2022-03-26 20:16:45 +0300

    Update

---
+++ Arrays/ArrayList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    public class ArrayList<T>
+    {
+        private T[] _storage;
+        private int _lastItemIndex;
+    }
+}
